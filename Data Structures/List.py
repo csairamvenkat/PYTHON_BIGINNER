e2e5f7326a2c192dc99b511065bed436cd82c921
csairamvenkat <mailtosairam.c@gmail.com>
2025-01-27 20:24:16 +0530

    DS Folder Created

---
+++ Data Structures/List.py
@@ -0,0 +1,35 @@
+# List can store diff data types
+#append-- to add end of list
+#copy
+#remove--1st occurance
+#reverse
+# l=[]
+#print(len(l)) #length
+# l.append(10)
+# l.append(20)
+# l.append(3.90)
+# l.append("Sai")
+# l.append(10+9j)
+# l.append(True)
+
+# l1=[1,2,3]
+# # print(l)
+# # print(id(l))
+# # print(id(l1))
+# l2=l1.copy()
+# print(l)
+# l1.remove(1)
+# print(l1)
+
+
+l=[1,2,3,4,5,6,7,8,9]
+l5=l.reverse()
+print(l)
+print(l5)
+
+# print(l[0:2])  #forward slicing
+# print(l[1:3])  #forward slicing
+# print(l[1:-3])  #backward slicing
+# print(l[1:8:2])  #step slicing
+# l[:] # all elements
+
