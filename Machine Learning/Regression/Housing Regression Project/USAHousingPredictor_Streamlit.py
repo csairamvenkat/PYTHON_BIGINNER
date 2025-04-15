90dcc931d8eb2edcc55d6c7b67594a2e0304fdf9
csairamvenkat <mailtosairam.c@gmail.com>
2025-04-15 20:05:20 +0530

    Implemented Flask

--- Machine Learning/Regression/Housing Regression Project/USAHousingPredictor_Streamlit.py
+++ Machine Learning/Regression/Housing Regression Project/USAHousingPredictor_Streamlit.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import USAHousingPredictor as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
